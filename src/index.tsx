2a8a32563f2bf7e7b798a3c0a6761a86812580de
wonder-SMA <wonder2012mail@gmail.com>
2022-03-13 21:52:17 +0400

    add changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ThemeContext, themes } from './components/Context/ThemeContext';
 import App from './components/App';
 import Navbar from './components/Navbar';
 import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <ThemeContext.Provider value={themes.dark}>
-      <Navbar />
-      <App />
-    </ ThemeContext.Provider>
+    <Navbar/>
+    <App/>
   </ BrowserRouter>,
   document.getElementById('root'),
 );
